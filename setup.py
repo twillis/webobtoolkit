1065a14e5897ec18748f162941d93908a5684c99
Tom Willis <twillis@topgunhq.com>
2014-03-08 15:29:40 -0500

    better out of the box testing via setuptools hotness

--- setup.py
+++ setup.py
@@ -1,26 +1,64 @@
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 from distutils.core import Command
 import os
+import sys
 
-version = '0.2.2'
+version = '0.2.3'
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 
-
-class PyTest(Command):
-    user_options = []
-
-    def initialize_options(self):
-        pass
+class PyTest(TestCommand):
+    test_package_name = 'webobtoolkit'
 
     def finalize_options(self):
-        pass
+        TestCommand.finalize_options(self)
+        _test_args = [
+            '--verbose',
+            '--ignore=build',
+            '--cov={0}'.format(self.test_package_name),
+            '--cov-report=term-missing',
+        ]
+        extra_args = os.environ.get('PYTEST_EXTRA_ARGS')
+        if extra_args is not None:
+            _test_args.extend(extra_args.split())
+        self.test_args = _test_args
+        self.test_suite = True
 
-    def run(self):
+    def run_tests(self):
         import pytest
-        errno = pytest.main("")
-        raise SystemExit(errno)
+        from pkg_resources import normalize_path, _namespace_packages
+
+        # Purge modules under test from sys.modules. The test loader will
+        # re-import them from the build location. Required when 2to3 is used
+        # with namespace packages.
+        if sys.version_info >= (3,) and getattr(self.distribution, 'use_2to3', False):
+            #module = self.test_args[-1].split('.')[0]
+            module = self.test_package_name
+            if module in _namespace_packages:
+                del_modules = []
+                if module in sys.modules:
+                    del_modules.append(module)
+                module += '.'
+                for name in sys.modules:
+                    if name.startswith(module):
+                        del_modules.append(name)
+                map(sys.modules.__delitem__, del_modules)
+
+            ## Run on the build directory for 2to3-built code
+            ## This will prevent the old 2.x code from being found
+            ## by py.test discovery mechanism, that apparently
+            ## ignores sys.path..
+            ei_cmd = self.get_finalized_command("egg_info")
+
+            ## Replace the module name with normalized path
+            #self.test_args[-1] = normalize_path(ei_cmd.egg_base)
+            self.test_args.append(normalize_path(ei_cmd.egg_base))
+
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
 
 setup(name='webobtoolkit',
       version=version,
@@ -44,8 +82,12 @@ setup(name='webobtoolkit',
           # -*- Extra requirements: -*-
           "Webob>=1.2.2"
       ],
+      tests_require = [
+          "pytest",
+          'pytest-cov',
+      ],
+      cmdclass={'test': PyTest},
       entry_points="""
       # -*- Entry points: -*-
       """,
-      cmdclass={"test": PyTest}
       )
